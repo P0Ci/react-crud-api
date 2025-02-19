997119717906312210cc58103d901e200ee94745
P0Ci <febriandry58@gmail.com>
2025-02-19 21:14:14 +0700

    First Commit

---
+++ src/routes/index.jsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "../views/home";
+import PostIndex from "../views/posts";
+import PostCreate from "../views/posts/create";
+import PostEdit from "../views/posts/edit";
+
+function RouteIndex() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/posts" element={<PostIndex />} />
+      <Route path="/posts/create" element={<PostCreate />} />
+      <Route path="/posts/edit" element={<PostEdit />} />
+    </Routes>
+  );
+}
+
+export default RouteIndex;
